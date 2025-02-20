a5fe4b67f306c547622b30e5a8e85168b4848e8f
Juan Reyes <jreyes@accipiterradar.com>
2025-02-20 11:57:46 -0500

    component setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { AboutMeComponent } from './web-components/about-me/about-me.component';
+import { ExperienceComponent } from './web-components/experience/experience.component';
+import { HeroComponent } from './web-components/hero/hero.component';
+import { ProjectsComponent } from './web-components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FooterComponent, HeaderComponent, AboutMeComponent, ExperienceComponent, HeroComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
